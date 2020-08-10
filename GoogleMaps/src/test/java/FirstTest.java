02f375d4514bd02b96bc1e4fe0a692cd66d8a955
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-08-10 22:16:17 +0530

    Google Maps update request

--- GoogleMaps/src/test/java/FirstTest.java
+++ GoogleMaps/src/test/java/FirstTest.java
@@ -1,9 +1,11 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import junit.framework.Assert;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import files.ReusableMethods;
 import files.payload;
 
 public class FirstTest {
@@ -26,30 +28,31 @@ public class FirstTest {
 		JsonPath js = new JsonPath(response);
 		String placeId = js.getString("place_id");
 		System.out.println("The placeid is - " + placeId);
-		
-		//-----------------------update place-------------------------
-		
-		String newAddress ="summer walking";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\r\n" + 
-				"\"place_id\":\""+placeId+"\",\r\n" + 
-				"\"address\":\""+newAddress+"\",\r\n" + 
-				"\"key\":\"qaclick123\"\r\n" + 
-				"}").when().put("maps/api/place/update/json")
-		.then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
-		
 
-		//-----------------------get place-------------------------
-		
-		String getPlaceResponse=given().log().all().queryParam("key", "qaclick123").
-		queryParam("place_id", placeId)
-		.when().get("maps/api/place/get/json")
-		.then().assertThat().log().all().statusCode(200).extract().response().asString();
-		
-		JsonPath js1 = new JsonPath(getPlaceResponse);
+		// -----------------------update place-------------------------
+
+		String newAddress = "summer walking";
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body("{\r\n" + "\"place_id\":\"" + placeId + "\",\r\n" + "\"address\":\"" + newAddress + "\",\r\n"
+						+ "\"key\":\"qaclick123\"\r\n" + "}")
+				.when().put("maps/api/place/update/json").then().assertThat().log().all().statusCode(200)
+				.body("msg", equalTo("Address successfully updated"));
+
+		// -----------------------get place-------------------------
+
+		String getPlaceResponse = given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeId)
+				.when().get("maps/api/place/get/json").then().assertThat().log().all().statusCode(200).extract()
+				.response().asString();
+
+		JsonPath js1 = ReusableMethods.rawToJson(getPlaceResponse);
+
+		// JsonPath js1 = new JsonPath(getPlaceResponse);
 		String actualAddress = js1.getString("address");
+
 		System.out.println("The adress is - " + actualAddress);
-		
+
+		Assert.assertEquals(actualAddress, newAddress);
+
 	}
 
 }
