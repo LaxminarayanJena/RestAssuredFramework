4c4222e4cf2601af0aaec95144d6d35fdcb2f3bf
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-09-10 01:07:48 +0530

    SpecsBuilder

---
+++ SpecBuilders/src/main/java/SpecBuilderTest.java
@@ -0,0 +1,49 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static  io.restassured.RestAssured.*;
+
+public class SpecBuilderTest {
+
+	public static void main(String[] args) {
+	
+		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
+		.setContentType(ContentType.JSON).build();
+		
+		RequestSpecification res=given().spec(req)
+		.body("{\r\n" + 
+				"  \"location\": {\r\n" + 
+				"    \"lat\": -38.383494,\r\n" + 
+				"    \"lng\": 33.427362\r\n" + 
+				"  },\r\n" + 
+				"  \"accuracy\": 50,\r\n" + 
+				"  \"name\": \"Frontline house\",\r\n" + 
+				"  \"phone_number\": \"(+91) 983 893 3937\",\r\n" + 
+				"  \"address\": \"29, side layout, cohen 09\",\r\n" + 
+				"  \"types\": [\r\n" + 
+				"    \"shoe park\",\r\n" + 
+				"    \"shop\"\r\n" + 
+				"  ],\r\n" + 
+				"  \"website\": \"http://google.com\",\r\n" + 
+				"  \"language\": \"French-IN\"\r\n" + 
+				"}");
+		
+		ResponseSpecification resspec= new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+		
+	Response response=	res.when().post("maps/api/place/add/json")
+		.then().spec(resspec)
+         .extract().response();
+	
+	String responseString =response.asString();
+	System.out.println(responseString);
+	
+		
+		
+
+	}
+
+}
