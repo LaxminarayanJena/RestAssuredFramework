8c694a9c237131bf44a64745d968b3f37b567117
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-09-12 20:30:47 +0530

    Added Global Properties

--- CucumberFramework/src/test/java/resources/Utils.java
+++ CucumberFramework/src/test/java/resources/Utils.java
@@ -1,8 +1,11 @@
 package resources;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Properties;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
@@ -12,20 +15,29 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 public class Utils {
+	RequestSpecification req ;
 	
-	public RequestSpecification requestSpecification() throws FileNotFoundException
+	public RequestSpecification requestSpecification() throws IOException
 	{
 		
 		PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
-		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		
 
-		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+		req = new RequestSpecBuilder().setBaseUri(getGlobalValue("baseUrl"))
 				.addQueryParam("key", "qaclick123")
 				.addFilter(RequestLoggingFilter.logRequestTo(log))
 				.addFilter(ResponseLoggingFilter.logResponseTo(log))
 				.setContentType(ContentType.JSON).build();
 		return req;
 
+	}
+	
+	public static String getGlobalValue(String key) throws IOException
+	{
+		Properties prop = new Properties();
+		FileInputStream fis= new FileInputStream("C:\\JENA\\RestAssured\\CucumberFramework\\src\\test\\java\\properties\\global.properties");
+		prop.load(fis);
+		return prop.getProperty(key);
 	}
 
 }
--- CucumberFramework/src/test/java/stepDefinations/AddPlace.java
+++ CucumberFramework/src/test/java/stepDefinations/AddPlace.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -25,7 +26,7 @@ public class AddPlace  extends Utils {
 	TestDataBuild data = new TestDataBuild();
 
 	@Given("Add Place Payload")
-	public void add_place_payload() throws FileNotFoundException  {
+	public void add_place_payload() throws IOException  {
 
 		
 		res = given().spec(requestSpecification()).body(data.addPlaceLoad());
