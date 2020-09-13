355554d698cd204c52627e48c122b91045d1e865
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-09-14 00:04:36 +0530

    DataDriven Testing

--- DataDriven/src/test/java/ExcelReading.java
+++ DataDriven/src/test/java/ExcelReading.java
@@ -1,30 +1,34 @@
 import static io.restassured.RestAssured.given;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
-public class HashMapToJSON {
+public class ExcelReading {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+
+		dataDriven d = new dataDriven();
+		ArrayList<String> data = d.getData("RestAddbook", "LibraryData");
+
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("name", "java");
-		map.put("isbn", "isbnjena99");
-		map.put("aisle", "685934");
-		map.put("author", "Jenaa");
+		map.put("name", data.get(1));
+		map.put("isbn", data.get(2));
+		map.put("aisle", data.get(3));
+		map.put("author", data.get(4));
 
-		//change isbn value before running
-		
+		// change all value before running
+		System.out.println(map);
+
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		String res = given().header("Content-Type", "application/json").body(map).when().post("/Library/Addbook.php")
 				.then().assertThat().statusCode(200).extract().response().asString();
 
 		JsonPath js = new JsonPath(res);
 		System.out.println(js.get("ID"));
-
 	}
-	
 
 }
---
+++ DataDriven/src/test/java/dataDriven.java
@@ -0,0 +1,57 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class dataDriven {
+
+	public ArrayList<String> getData(String testcaseName, String sheetName) throws IOException {
+		ArrayList<String> a = new ArrayList<String>();
+		FileInputStream fis = new FileInputStream(
+				"C://JENA//RestAssured//DataDriven//src//test//resources//demodata.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+		int sheets = workbook.getNumberOfSheets();
+		for (int i = 0; i < sheets; i++) {
+			if (workbook.getSheetName(i).equalsIgnoreCase(sheetName)) {
+				XSSFSheet sheet = workbook.getSheetAt(i);
+				Iterator<Row> rows = sheet.iterator();
+				Row firstrow = rows.next();
+				Iterator<Cell> ce = firstrow.cellIterator();
+				int k = 0;
+				int column = 0;
+				while (ce.hasNext()) {
+					Cell value = ce.next();
+					if (value.getStringCellValue().equalsIgnoreCase("Testcases")) {
+						column = k;
+					}
+					k++;
+				}
+
+				while (rows.hasNext()) {
+					Row r = rows.next();
+					if (r.getCell(column).getStringCellValue().equalsIgnoreCase(testcaseName)) {
+						Iterator<Cell> cv = r.cellIterator();
+						while (cv.hasNext()) {
+							Cell c = cv.next();
+							if (c.getCellTypeEnum() == CellType.STRING) {
+								a.add(c.getStringCellValue());
+							} else {
+								a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+							}
+						}
+					}
+				}
+			}
+		}
+		return a;
+	}
+
+}
