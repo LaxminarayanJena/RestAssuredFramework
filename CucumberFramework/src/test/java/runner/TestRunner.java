84bbbc7e3cf3726ce43e682313fcbe1be1729dd7
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-09-13 11:26:24 +0530

    Cucumber Maven Reporting

--- CucumberFramework/src/test/java/runner/TestRunner.java
+++ CucumberFramework/src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 //@CucumberOptions(features="src/test/java/features",glue= {"stepDefinations"},tags= "@DeletePlace")
-@CucumberOptions(features="src/test/java/features",glue= {"stepDefinations"})
+@CucumberOptions(features="src/test/java/features", plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepDefinations"})
 public class TestRunner {
 	
 
